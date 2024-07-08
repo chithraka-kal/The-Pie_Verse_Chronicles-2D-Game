5926cbaac76967cd48b356fd63fe2f594ce313fd
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2024-07-09 03:11:03 +0530

    Player Update
    Walk, Run, Idle Animations and Flip Direction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,15 +8,57 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float walkSpeed = 5f;
+    public float runSpeed = 8f;
     Vector2 moveInput;
 
-    public bool IsMoving { get; private set; }
+    public float CurrentMoveSpeed { get
+    {
+        if(IsMoving){
+            if(IsRunning){
+                return runSpeed;
+            }else{
+                return walkSpeed;
+            }
+        }else{
+            //idele speed is zero
+            return 0;
+        }
+    }}
+
+    [SerializeField] private bool _isMoving = false;
+
+    public bool IsMoving { get
+    {
+        return _isMoving;
+    }
+    private set
+    {
+        _isMoving = value;
+        animator.SetBool("isMoving", value);
+    }
+    } 
+
+    [SerializeField] private bool _isRunning = false;
+    public bool IsRunning
+    {
+        get
+        {
+            return _isRunning;
+        }
+        set
+        {
+            _isRunning = value;
+            animator.SetBool("isRunning", value);
+        }
+    }
 
     Rigidbody2D rb;
+    Animator animator;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
@@ -33,12 +75,31 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(moveInput.x * walkSpeed, rb.velocity.y);
+        rb.velocity = new Vector2(moveInput.x * CurrentMoveSpeed, rb.velocity.y);
     }
 
     public void onMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
         IsMoving = moveInput != Vector2.zero;
+        SetFacingDirection(moveInput);
+    }
+
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if(moveInput.x > 0){
+            transform.localScale = new Vector2(1, 1);
+        } else if(moveInput.x < 0){
+            transform.localScale = new Vector2(-1, 1);
+        }
+    }
+
+    public void onRun(InputAction.CallbackContext context)
+    {
+        if (context.started){
+            IsRunning = true;
+        }else if (context.canceled){
+            
+        }
     }
 }
