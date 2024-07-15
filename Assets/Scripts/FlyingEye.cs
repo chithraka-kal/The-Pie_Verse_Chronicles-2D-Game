ef6d2afd4c4e39fe391f661c0a01b172f16f1379
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2024-07-16 02:05:32 +0530

    Enemy update
    flying enemy has waypoints

--- Assets/Scripts/FlyingEye.cs
+++ Assets/Scripts/FlyingEye.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class FlyingEye : MonoBehaviour
 {
-    public float flightSpeed = 3f;
+    public float flightSpeed = 2f;
+    public float waypointReachedDistance = 0.1f;    
     public DetectionZone biteDetectionZone;
+    public List<Transform> waypoints;
+
     Animator animator;
     Rigidbody2D rb;
+    Damageable damageable;
+    Transform nextWaypoint;
+    int waypointNum = 0;
 
     public bool _hasTarget = false;
 
@@ -22,15 +28,68 @@ public class FlyingEye : MonoBehaviour
         }
     }
 
+    public bool CanMove
+    {
+        get
+        {
+           return animator.GetBool(AnimationStrings.canMove);
+        }
+    }    
+
     private void Awake()
     {
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        damageable = GetComponent<Damageable>();
+    }
+
+    private void Start()
+    {
+       nextWaypoint = waypoints[waypointNum]; 
     }
 
     // Update is called once per frame
     void Update()
     {
         HasTarget = biteDetectionZone.detectedColliders.Count > 0;  
+    }
+
+    private void FixedUpdate()
+    {
+        if(damageable.IsAlive)
+        {
+            if(CanMove)
+            {
+                Flight();
+            }
+            else{
+                rb.velocity = Vector3.zero;
+            }
+        }
+    }
+
+    private void Flight()
+    {
+        //fly to the next waypoint
+        Vector2 directionToWaypoint = (nextWaypoint.position - transform.position).normalized;
+
+        //Check reached the waypoint already
+        float distance = Vector2.Distance(nextWaypoint.position, transform.position);
+        rb.velocity = directionToWaypoint * flightSpeed;
+
+        //see if need to switch waypoints
+        if(distance <= waypointReachedDistance)
+        {
+            //switch to next waypoint
+            waypointNum++;
+
+            if(waypointNum >= waypoints.Count)
+            {
+                //loop back to original waypoint
+                waypointNum = 0;
+            }
+
+            nextWaypoint = waypoints[waypointNum];
+        }
     }
 }
