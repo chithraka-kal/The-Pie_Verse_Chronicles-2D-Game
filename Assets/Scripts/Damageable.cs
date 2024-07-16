856a7c225e6a349451d061d1293e37fbaa268f72
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2024-07-16 22:47:46 +0530

    Player Update
    Health UI bar added

--- Assets/Scripts/Damageable.cs
+++ Assets/Scripts/Damageable.cs
@@ -7,6 +7,7 @@ public class Damageable : MonoBehaviour
 {
     public UnityEvent<int, Vector2> damageableHit;
     public UnityEvent damageableDeath;
+    public UnityEvent<int, int> healthChanged;
 
     Animator animator;
     [SerializeField]
@@ -36,6 +37,7 @@ public class Damageable : MonoBehaviour
         set
         {
             _health = value;
+            healthChanged?.Invoke(_health, _maxHealth);
 
             // If health drops below 0, character is no longer alive
             if(_health <= 0)
--- Assets/Scripts/HealthPickup.cs
+++ Assets/Scripts/HealthPickup.cs
@@ -2,27 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class HealthPickup : MonoBehaviour
 {
     public int healthRestore = 20;
     public Vector3 spinRotationSpeed = new Vector3(0, 180, 0);
 
-    // Start is called before the first frame update
-    void Start()
+    AudioSource pickupSource;
+
+    private void Awake()
     {
-        
+        pickupSource = GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Damageable damageable = collision.GetComponent<Damageable>();
 
-        if(damageable)
+        if (damageable && damageable.Health < damageable.MaxHealth)
         {
             bool wasHealed = damageable.Heal(healthRestore);
-            
-            if(wasHealed)
+
+            if (wasHealed)
+            {
+                if (pickupSource)
+                {
+                    AudioSource.PlayClipAtPoint(pickupSource.clip, gameObject.transform.position, pickupSource.volume);
+                }
                 Destroy(gameObject); // destroy the health pickup when collected
+            }
         }
     }
 
