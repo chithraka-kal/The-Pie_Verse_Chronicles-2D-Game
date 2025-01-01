f746e122fa421a9191515a8c6bc75e45f5ca67d2
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-01-01 11:11:10 +0530

    traps-cristal

---
+++ Assets/Traps.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Traps : MonoBehaviour
+{
+    [SerializeField] private int damage = 10; // Default damage dealt by the trap
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Check if the object can be damaged
+        Damageable damageable = collision.GetComponent<Damageable>();
+        if (damageable != null)
+        {
+            damageable.Hit(damage, Vector2.zero); // Apply damage with no knockback
+        }
+    }
+}
