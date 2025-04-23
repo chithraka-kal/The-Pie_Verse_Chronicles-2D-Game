7392c89e27ee4c1488accde6c8cbf2ce9755d80c
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-04-23 23:19:01 +0530

    npc

--- Assets/Scripts/DialogSystem/Dialog.cs
+++ Assets/Scripts/DialogSystem/Dialog.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
@@ -9,38 +8,61 @@ public class Dialog : MonoBehaviour
     public string[] sentences;
     private int index;
     public float typingSpeed;
-
     public GameObject continueButton;
+
+    private Coroutine typingCoroutine;
+    private bool isTyping = false;
 
     void Start()
     {
-        StartCoroutine(Type());
+        if (sentences.Length > 0)
+        {
+            typingCoroutine = StartCoroutine(Type());
+        }
+        else
+        {
+            Debug.LogWarning("No sentences assigned!");
+        }
     }
 
     void Update()
     {
-        if (dialogText.text == sentences[index]){
+        if (!isTyping && dialogText.text == sentences[index])
+        {
             continueButton.SetActive(true);
-        } 
-    }
-
-    IEnumerator Type(){
-         
-        foreach (char letter in sentences[index].ToCharArray()){
-            dialogText.text += letter;
-            yield return new WaitForSeconds(typingSpeed);
         }
     }
 
-    public void NextSentence(){
+    IEnumerator Type()
+    {
+        isTyping = true;
+        dialogText.text = "";
+        continueButton.SetActive(false);
+
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            dialogText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+
+        isTyping = false;
+        continueButton.SetActive(true);
+    }
+
+    public void NextSentence()
+    {
+        if (typingCoroutine != null)
+            StopCoroutine(typingCoroutine);
 
         continueButton.SetActive(false);
 
-        if (index < sentences.Length - 1){
+        if (index < sentences.Length - 1)
+        {
             index++;
-            dialogText.text = "";
-            StartCoroutine(Type());
-        } else {
+            typingCoroutine = StartCoroutine(Type());
+        }
+        else
+        {
             dialogText.text = "";
             continueButton.SetActive(false);
         }
