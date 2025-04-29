feddf16c6e7c3c48e91c8f683983d00a18102dd1
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-04-30 03:56:54 +0530

    main menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,11 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    public GameObject mainMenuPanel;
+    public GameObject levelSelectPanel;
+    public GameObject aboutPanel;
+
+    public int gameStartScene;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(gameStartScene);
+    }
+
+    public void OpenLevels()
+    {
+        mainMenuPanel.SetActive(false);
+        levelSelectPanel.SetActive(true);
+    }
+
+    public void OpenAbout()
+    {
+        mainMenuPanel.SetActive(false);
+        aboutPanel.SetActive(true);
+    }
+
+    public void BackToMenu()
+    {
+        levelSelectPanel.SetActive(false);
+        aboutPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+
     public void QuitGame()
     {
         Application.Quit();
+        Debug.Log("Quit Game"); // Only visible in Editor
+    }
+
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
     }
 }
