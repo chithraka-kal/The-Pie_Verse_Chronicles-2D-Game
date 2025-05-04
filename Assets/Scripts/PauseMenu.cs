90accbd5f154167be66257659634449b5872f5b7
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-05-04 19:37:21 +0530

    pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    public GameObject soundObject;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+            Debug.Log("Escape key pressed. Toggling pause menu.");
+        }
+    }
+
+    void TogglePause()
+    {
+        if (gameIsPaused)
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+        soundObject.SetActive(true); 
+        //AudioListener.pause = false; // Resume the audio listener
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+        soundObject.SetActive(false);
+        //AudioListener.pause = true; // Pause the audio listener 
+    }
+}
