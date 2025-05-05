000512d17e505d82a49ec053e31c420214915691
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-05-06 04:36:05 +0530

    abt panel activator

--- Assets/Scripts/DialogSystem/DBox1/DialogueTrigger.cs
+++ Assets/Scripts/DialogSystem/DBox1/DialogueTrigger.cs
@@ -1,13 +1,13 @@
 using System.Collections.Generic;
 using UnityEngine;
- 
+
 [System.Serializable]
 public class DialogueCharacter
 {
     public string name;
     public Sprite icon;
 }
- 
+
 [System.Serializable]
 public class DialogueLine
 {
@@ -15,30 +15,32 @@ public class DialogueLine
     [TextArea(3, 10)]
     public string line;
 }
- 
+
 [System.Serializable]
 public class Dialogue
 {
     public List<DialogueLine> dialogueLines = new List<DialogueLine>();
 }
- 
+
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
     private bool hasTriggered = false;
 
- 
+    public bool isFinalDialogue = false; // ← checkbox in inspector
+    public GameObject aboutPanel;        // ← assign if isFinalDialogue is true
+
     public void TriggerDialogue()
     {
-        DialogueManager.Instance.StartDialogue(dialogue);
+        DialogueManager.Instance.StartDialogue(dialogue, isFinalDialogue, aboutPanel);
     }
- 
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!hasTriggered && collision.CompareTag("Player"))
-    {
-        hasTriggered = true;
-        TriggerDialogue();
-    }
+        {
+            hasTriggered = true;
+            TriggerDialogue();
+        }
     }
 }
