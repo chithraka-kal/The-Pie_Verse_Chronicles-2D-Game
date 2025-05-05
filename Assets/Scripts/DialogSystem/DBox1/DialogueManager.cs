7f8762fbfd63665272dd36129f4dce9ce02e636e
Chithraka Kalanamith <chithrakakalanamith@gmail.com>
2025-05-06 04:36:36 +0530

    dialog manager

--- Assets/Scripts/DialogSystem/DBox1/DialogueManager.cs
+++ Assets/Scripts/DialogSystem/DBox1/DialogueManager.cs
@@ -3,91 +3,97 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 using TMPro;
- 
+
 public class DialogueManager : MonoBehaviour
 {
     public static DialogueManager Instance;
- 
+
     public Image characterIcon;
     public TextMeshProUGUI characterName;
     public TextMeshProUGUI dialogueArea;
- 
+
     private Queue<DialogueLine> lines;
-    
+
     public bool isDialogueActive = false;
- 
+
     public float typingSpeed = 0.2f;
- 
+
     public Animator animator;
 
     private AudioSource source;
+
+    private bool shouldShowAboutPanel = false;
+    private GameObject aboutPanelToShow;
 
     void Start()
     {
         source = GetComponent<AudioSource>();
     }
- 
+
     private void Awake()
     {
         if (Instance == null)
             Instance = this;
- 
+
         lines = new Queue<DialogueLine>();
     }
- 
-    public void StartDialogue(Dialogue dialogue)
+
+    public void StartDialogue(Dialogue dialogue, bool isFinal = false, GameObject aboutPanel = null)
     {
         isDialogueActive = true;
- 
+
         animator.Play("show");
- 
+
         lines.Clear();
- 
+
+        shouldShowAboutPanel = isFinal;
+        aboutPanelToShow = aboutPanel;
+
         foreach (DialogueLine dialogueLine in dialogue.dialogueLines)
         {
             lines.Enqueue(dialogueLine);
         }
- 
+
         DisplayNextDialogueLine();
     }
- 
+
     public void DisplayNextDialogueLine()
     {
         source.Play();
+
         if (lines.Count == 0)
         {
             EndDialogue();
             return;
         }
- 
+
         DialogueLine currentLine = lines.Dequeue();
- 
+
         if (currentLine.character != null)
-{
-    if (currentLine.character.icon != null)
-    {
-        characterIcon.gameObject.SetActive(true);
-        characterIcon.sprite = currentLine.character.icon;
-    }
-    else
-    {
-        characterIcon.gameObject.SetActive(false);
-    }
+        {
+            if (currentLine.character.icon != null)
+            {
+                characterIcon.gameObject.SetActive(true);
+                characterIcon.sprite = currentLine.character.icon;
+            }
+            else
+            {
+                characterIcon.gameObject.SetActive(false);
+            }
 
-    characterName.text = currentLine.character.name;
-}
-else
-{
-    characterIcon.gameObject.SetActive(false);
-    characterName.text = "";
-}
+            characterName.text = currentLine.character.name;
+        }
+        else
+        {
+            characterIcon.gameObject.SetActive(false);
+            characterName.text = "";
+        }
 
- 
         StopAllCoroutines();
- 
+
         StartCoroutine(TypeSentence(currentLine));
     }
- 
+
     IEnumerator TypeSentence(DialogueLine dialogueLine)
     {
         dialogueArea.text = "";
@@ -97,13 +103,16 @@ else
             yield return new WaitForSeconds(typingSpeed);
         }
     }
- 
- public static event System.Action OnDialogueEnded;
+
     void EndDialogue()
     {
         isDialogueActive = false;
         animator.Play("hide");
 
-        OnDialogueEnded?.Invoke();
+        if (shouldShowAboutPanel && aboutPanelToShow != null)
+        {
+            aboutPanelToShow.SetActive(true);
+            Debug.Log("Final dialogue ended. About Panel shown.");
+        }
     }
 }
